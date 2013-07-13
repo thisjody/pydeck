c258aca513896dc87bce09d111a01a6f3146f901
jody <thisjody@gmail.com>
2013-07-12 17:54:16 -0700

    finishing the OldMaidGame code

--- pydeck.py
+++ pydeck.py
@@ -125,4 +125,44 @@ class OldMaidGame(CardGame):
         # deal the cards
         self.deck.deal(self.hands)
         print "---------- Cards have been dealt"
-        self.printHands()
+        #self.printHands()
+
+        # remove initial matches
+        matches = self.removeAllMatches()
+        print "---------- Matches discarded, play begins"
+        #self.printHands()
+
+        # play until all 50 cards are matched
+        turn = 0
+        numHands = len(self.hands)
+        while matches < 25:
+            matches = matches + self.playOneTurn(turn)
+            turn = (turn + 1) % numHands
+
+        print "---------- Game is Over"
+        #self.printHands()
+
+    def removeAllMatches(self):
+        count = 0
+        for hand in self.hands:
+            count = count + hand.removeMatches()
+            return count
+
+    def playOneTurn(self, i):
+        if self.hands[i].isEmpty():
+            return 0
+        neighbor = self.findNeighbor(i)
+        pickedCard = self.hands[neighbor].popCard()
+        self.hands[i].addCard(pickedCard)
+        print "Hand", self.hands[i].name, "picked", pickedCard
+        count = self.hands[i].removeMatches()
+        self.hands[i].shuffle()
+        return count
+
+
+    def findNeighbor(self, i):
+        numHands = len(self.hands)
+        for next in range(1,numHands):
+            neighbor = (i + next) % numHands
+            if not self.hands[neighbor].isEmpty():
+                return neighbor
